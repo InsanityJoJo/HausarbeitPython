239dbd0637ef224cb78358df080e3af941cec164
InsanityJoJo <LiebJoh@protonmail.com>
2024-02-18 13:00:47 +0100

    Erstellen und Verfeinern der Klasse Dataloader mit erster Fehlerbehandlung. Im Sinne des TDD werden die Tests mit-commited

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,14 +1,39 @@
 import pandas as pd
+import os
 
 class DataLoader:
     ''' 
-    Diese Klasse läd aus die Daten aus den CSV-Dateien.
+    Diese Klasse enhält Methoden für:
+    - das Auswählen der Datei über den Pfad
+    - das Lesen der Daten aus der CSV-Datei
     '''
     def __init__(self, file_path):
         '''
-        Diese Methode legt den Pfad der CSV-Datei fest
+        Diese Methode legt den Pfad der CSV-Datei fest. Sie stellt sicher, dass die Datei existiert.
+
+        Methodenparameter:
+        - self
+        - file_path
         '''
-        self.file_path = file_path
-    
+        # Sollte die Datei nicht existieren, wird eine FileNotFoundError geworfen.
+        if not os.path.exists(file_path):
+            # raised den FileNotFoundError mit eigener Nachricht.
+            raise FileNotFoundError(f"Die Datei {file_path} konnte nicht gefunden werden")
+            
+        else:
+            
+            self.file_path = file_path
+
     def load_data(self):
+        '''
+        Diese Methode ermöglicht das lesen der CSV-Datei.
+        Methodenparameter:
+        - self
+        
+        Rückgabewert:
+        - Dataframe aus den Daten der CSV-Datei
+        '''
+
+
         return pd.read_csv(self.file_path)
+
--- tests/test_loading.py
+++ tests/test_loading.py
@@ -3,13 +3,25 @@ from src.data_loading import DataLoader
 '''
 Dieser Test soll sicherstellen, dass ein Laden der Daten aus einer CSV möglichlich ist.
 '''
-def test_can_load_csv_data():
+def test_load_data():
     '''
-    Test des Dataloaders in src/data_loading.py 
+    Test des Dataloaders in src/data_loading.py
+    Testfälle: 
+    - Kann eine bestehende CSV-Datei geladen werden
+    - ist die geladene Datei ein Padas Dataframe
+    - Exception (FileNotFoundError) mit eigener Nachricht, wenn die Datei nicht existiert     
     '''
+    # laden einer des Beispieldatensatzes der Trainingsdaten
     loader = DataLoader("data/example_data/train.csv")
     data = loader.load_data()
+    # Check, dass Daten geladen wurden
     assert data is not None
+    # Check, dass Daten nicht leer sind.
     assert len(data) > 0
 
-    
+    # Dieser Testfall prüft den FileNotFoundError bei nicht existierenden Dateien.    
+    with pytest.raises(FileNotFoundError) as exc_info:
+        loader2 = DataLoader("non_existent_file.csv")
+        data = loader2.load_data()
+    # Check, dass hier die erwatete Fehlermedung übergeben wurde.
+    assert "Die Datei non_existent_file.csv konnte nicht gefunden werden" in str(exc_info.value)
