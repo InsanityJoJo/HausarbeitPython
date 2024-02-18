831e48efda253525aa18cfb8474c5f9ee24e5898
InsanityJoJo <LiebJoh@protonmail.com>
2024-02-18 17:42:13 +0100

    Validierung des Formats der CSV hinzugefügt. Tests für diese Funktion hinzugefügt.

--- src/data_loading.py
+++ src/data_loading.py
@@ -19,7 +19,11 @@ class DataLoader:
         if not os.path.exists(file_path):
             # raised den FileNotFoundError mit eigener Nachricht.
             raise FileNotFoundError(f"Die Datei {file_path} konnte nicht gefunden werden")
-            
+
+        # Sollte die Datei nicht auf .csv enden, dann wird ein TypeError geworfen.
+        elif not file_path[-4:] == ".csv":
+            # wirft den TypeError mit eigener Nachricht
+            raise TypeError(f"Die Datei {file_path} ist nicht vom Typ .csv")
         else:
             
             self.file_path = file_path
@@ -33,7 +37,28 @@ class DataLoader:
         Rückgabewert:
         - Dataframe aus den Daten der CSV-Datei
         '''
-
-
         return pd.read_csv(self.file_path)
 
+    def validate_csv_format(self, df):
+        '''
+        Diese Methode ist für die Validierung des Formats der aus den CSVs erstellten Dataframes zuständig.
+        Es wird überprüft ob die Daten dem Format der Trainigs, Ideal oder Testdaten entsprechen. 
+        Die erlaubten Spalten sind in diesen Sets hart einprogrammiert, da laut Aufgabenstellung nur solche Daten zu erwarten sind.
+        
+        Die Validierung ist optional, da das Programm auch mit anderen Formaten arbeiten könnte.
+        Wichtig ist nur, dass die x-Werte in der esten Spalte stehen.
+
+        Ist das Format nicht das der Beispieldatensätze, so wird ein ValueError mit eigener Nachricht übergeben.
+        
+        Methodenparameter:
+        - df: Dataframe, dass durch das Programm verwendet werden soll
+        '''
+
+        # Sets der erlaubten Spalten        
+        valid_columns_lists = [
+            ['x', 'y'],  # Format der Testdaten
+            ['x', 'y1', 'y2', 'y3', 'y4'],  # Format der Trainingsdaten
+            ['x'] + [f'y{i}' for i in range(1, 51)]  # Format der idealen Funktionen
+        ]
+        if not any(df.columns.tolist() == valid_columns for valid_columns in valid_columns_lists):
+            raise ValueError("Die CSV-Datei hat nicht das richtige Format.")
--- tests/test_loading.py
+++ tests/test_loading.py
@@ -1,4 +1,5 @@
 import pytest
+import pandas as pd
 from src.data_loading import DataLoader
 '''
 Dieser Test soll sicherstellen, dass ein Laden der Daten aus einer CSV möglichlich ist.
@@ -9,19 +10,40 @@ def test_load_data():
     Testfälle: 
     - Kann eine bestehende CSV-Datei geladen werden
     - ist die geladene Datei ein Padas Dataframe
-    - Exception (FileNotFoundError) mit eigener Nachricht, wenn die Datei nicht existiert     
+    - Exception (FileNotFoundError) mit eigener Nachricht, wenn die Datei nicht existiert.
+    - Exception (TypeError) mit eigener Nachricht, wenn die Datei keine Endung mit .csv hat.     
     '''
     # laden einer des Beispieldatensatzes der Trainingsdaten
     loader = DataLoader("data/example_data/train.csv")
-    data = loader.load_data()
+    data1 = loader.load_data()
+
     # Check, dass Daten geladen wurden
-    assert data is not None
+    assert data1 is not None
+    
     # Check, dass Daten nicht leer sind.
-    assert len(data) > 0
+    assert len(data1) > 0
+    
+    # Check, dass die Daten vom type Dataframe sind
+    assert type(data1) == pd.DataFrame
 
     # Dieser Testfall prüft den FileNotFoundError bei nicht existierenden Dateien.    
-    with pytest.raises(FileNotFoundError) as exc_info:
+    with pytest.raises(FileNotFoundError) as fnfexc_info:
         loader2 = DataLoader("non_existent_file.csv")
-        data = loader2.load_data()
+        data2 = loader2.load_data()
     # Check, dass hier die erwatete Fehlermedung übergeben wurde.
-    assert "Die Datei non_existent_file.csv konnte nicht gefunden werden" in str(exc_info.value)
+    assert "Die Datei non_existent_file.csv konnte nicht gefunden werden" in str(fnfexc_info.value)
+
+    # Dier Testfall prüft den TypeEror der geworfen werden soll, wenn eine nicht-csv Datei geladen werden soll.
+    with pytest.raises(TypeError) as teexc_info:
+        loader3 = DataLoader("data/example_data/textdatei.txt")
+        data3 = loader3.load_data()
+    # Check, dass hier die erwatete Fehlermedung übergeben wurde.
+    assert "Die Datei data/example_data/textdatei.txt ist nicht vom Typ .csv" in str(teexc_info.value)
+
+    # Dieser Testfall überprüft, den ValueError der geworfen werden soll, wenn eine CSV-Datei nicht das gewünschte Format hat.
+    with pytest.raises(ValueError) as veexc_info:
+        loader4 = DataLoader("data/example_data/wrongformat.csv")
+        data4 = loader4.load_data()
+        loader4.validate_csv_format(data4)
+
+    assert "Die CSV-Datei hat nicht das richtige Format." in str(veexc_info.value)
